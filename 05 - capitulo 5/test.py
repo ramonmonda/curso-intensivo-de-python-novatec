85ab1352b28c11e6dae1053f9ec2890bd752b143
Ramon Monda <ramondriguess@gmail.com>
2025-02-25 08:56:23 -0300

    Update Capítulo 5.

---
+++ 05 - capitulo 5/test.py
@@ -0,0 +1,35 @@
+# Testes Capítulo 5
+# ----------------------------------------------
+
+
+# Faça você mesmo página 116
+# ----------------------------------------------
+
+# car = "subaru"
+# print("O car == 'subaru'? Eu acho que sim.")
+# print(car == "subaru")
+
+# print("\nO car == 'audi'? Eu acho que não.")
+# print(car == "audi")
+
+# ----------------------------------------------
+
+
+# Faça você mesmo página 124
+# ----------------------------------------------
+
+# alien_color = "verde"
+
+# if alien_color == "verde":
+#     print(f"O alien é {alien_color}!")
+
+# ----------------------------------------------
+
+
+
+
+
+
+
+
+
