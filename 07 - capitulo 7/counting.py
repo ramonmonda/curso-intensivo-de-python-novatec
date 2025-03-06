047f3f88143be10ff5368d4538264813b333518b
Ramon Monda <ramondriguess@gmail.com>
2025-03-06 10:56:13 -0300

    Update Capítulo 7.

---
+++ 07 - capitulo 7/counting.py
@@ -0,0 +1,7 @@
+
+numero_atual = 1
+
+while numero_atual <= 5:
+    print(numero_atual)
+    numero_atual += 1
+
--- 07 - capitulo 7/test.py
+++ 07 - capitulo 7/test.py
@@ -2,6 +2,35 @@
 # ----------------------------------------------
 
 
-# Faça você mesmo página 
+# Faça você mesmo página 161
 # ----------------------------------------------
+
+# 7.1
+# carro = input("Que tipo de carro você gosta de alugar? ")
+# print(f"Vejamos se consigo arrumar um {carro.title()} para você.")
+
+# 7.2
+# reserva = int(input("Quantos lugares na mesa você precisa? "))
+
+# if reserva > 8:
+#     print("É necessário aguardar uma mesa!")
+# else:
+#     print("A mesa já está disponível!")
+
+# 7.3
+# numero_informado = int(input("Digite um número: "))
+
+# if numero_informado % 10 == 0:
+#     print(f"O número {numero_informado} é multíplo de 10.")
+# else:
+#     print(f"O número {numero_informado} não é multíplo de 10.")
+
+# ----------------------------------------------
+
+
+
+
+
+
+
 # ----------------------------------------------
