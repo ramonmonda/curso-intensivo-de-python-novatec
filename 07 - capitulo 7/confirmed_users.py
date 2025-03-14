792bc9b4e53c6c168194d5f16b9ac6d99e4b56eb
Ramon Monda <ramondriguess@gmail.com>
2025-03-14 13:06:06 -0300

    Update Capítulo 7

---
+++ 07 - capitulo 7/confirmed_users.py
@@ -0,0 +1,16 @@
+
+usuarios_nao_confirmados = [
+    "alice", "brian", "candace",
+]
+
+usuarios_confirmados = []
+
+while usuarios_nao_confirmados:
+    usuario_atual = usuarios_nao_confirmados.pop()
+
+
+
+
+
+
+
--- 07 - capitulo 7/test.py
+++ 07 - capitulo 7/test.py
@@ -28,9 +28,61 @@
 # ----------------------------------------------
 
 
+# Faça você mesmo página 168
+# ----------------------------------------------
 
+# 7.4
+# ingredientes = []
+# print("\nOlá, vamos adicionar alguns ingredientes a sua pizza!")
+# print("Digite o nome dos ingredientes ou 'quit' para terminar...\n")
+# ordem = 1
 
+# while True:
+#     ingrediente = str(input(f"{ordem}° ingrediente: "))
 
+#     if ingrediente == "quit":
+#         break
+#     else:
+#         ingredientes.append(ingrediente)
+#         ordem += 1
 
+# print(f"\nOs ingredientes são: {ingredientes}.\n")
+
+# 7.5
+# print("\nOlá, o valor dos ingressos depende da idade da pessoa!")
+
+# while True:
+#     idade_pessoa = input("Qual a sua idade?('quit' para sair) ")
+
+#     if idade_pessoa == "quit":
+#         break
+#     else:
+#         idade_pessoa = int(idade_pessoa)
+
+#         if idade_pessoa < 3:
+#             print("O ingresso é gratuito.\n")
+#         elif idade_pessoa >= 3 and idade_pessoa <= 12:
+#             print("O ingresso custa US$ 10.\n")
+#         elif idade_pessoa > 12:
+#             print("O ingresso custa US$ 15.\n")
+
+# 7.7
+# cont = 0
+
+# while True:
+#     print(f"{cont}", end="\t")
+#     cont += 1
 
 # ----------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
